9b81c66d0e436d5b631210961d0b5dc017e9c73e
Evan Burkey <Evan Burkey>
2021-12-31 10:31:02 -0800

    Sort raiders properly

--- pkg/raiders/raider.go
+++ pkg/raiders/raider.go
@@ -11,9 +11,9 @@ type Raider struct {
 func SortSlice(rs []Raider) {
 	sort.Slice(rs, func(i, j int) bool {
 		if rs[i].Points < rs[j].Points {
-			return true
-		} else if rs[i].Points > rs[j].Points {
 			return false
+		} else if rs[i].Points > rs[j].Points {
+			return true
 		}
 		return rs[i].Name[0] < rs[j].Name[0]
 	})
